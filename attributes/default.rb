b36bb110a3246211a5128f30295969f18e396459
Mayuri <123patilmayuri>
2016-07-22 16:20:55 +0530

    update in attr & recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,12 @@
-default['tomcat']['base_version'] = 6
-default['tomcat']['port'] = 8080
+default['tomcat-install']['base_version'] = 6
+default['tomcat-install']['port'] = 8080
+default['tomcat-install']['shutdown_port'] = 8005
+default['tomcat-install']['ssl_port'] = 8443
+default['tomcat-install']['ssl_max_threads'] = 150
+default['tomcat-install']['deploy_manager_apps'] = true
+default['tomcat-install']['use_security_manager'] = false
+default['tomcat-install']['tomcat_auth'] = true
+default['tomcat-install']['run_base_instance'] = true
+
+
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,16 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.default['tomcat']['base_version'] = node['tomcat-install']['port']
+node.default['tomcat']['base_version'] = node['tomcat-install']['base_version']
 node.default['tomcat']['port'] = node['tomcat-install']['port']
+node.default['tomcat']['shutdown_port'] = node['tomcat-install']['shutdown_port']
+node.default['tomcat']['ssl_port'] = node['tomcat-install']['ssl_port']
+node.default['tomcat']['ssl_max_threads'] = node['tomcat-install']['ssl_max_threads ']
+node.default['tomcat']['deploy_manager_apps'] = node['tomcat-install']['deploy_manager_apps']
+node.default['tomcat']['use_security_manager'] = node['tomcat-install']['use_security_manager']
+node.default['tomcat']['tomcat_auth'] = node['tomcat-install']['tomcat_auth'] 
+node.default['tomcat']['run_base_instance'] = node['tomcat-install']['run_base_instance'] 
 
 case node['platform_family']
   when 'debian'
     apt_update 'apt update' do
       action :update
     end
-    
-    if ['tomcat-install']['base_version'] == 6
+
+    if node['tomcat-install']['base_version'] == 6
       node.default["java"]["java_home"] = "/usr/lib/jvm/jdk-oracle.1.6.0_33"
     end
 end
